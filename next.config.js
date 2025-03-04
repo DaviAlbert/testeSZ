39d40ebc1fadf6a8595f09ec720c333d3327c4b6
Davi Hoffmann Takahashi Albert <david.albert@szsolucoes.com.br>
2025-03-03 21:28:14 -0300

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  pageExtensions: ['page.tsx', 'api.ts', 'api.tsx'],
-}
+  swcMinify: true, // Habilita minificação com SWC para melhor performance
+  pageExtensions: ['page.tsx', 'api.ts', 'api.tsx', 'tsx', 'ts', 'js', 'jsx'], // Extensões suportadas para páginas e APIs
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
